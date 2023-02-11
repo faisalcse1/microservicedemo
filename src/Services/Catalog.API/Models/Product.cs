922bfcb367473563c59028d773ea51fa3020456d
MD FAISAL <faisalcse1@gmail.com>
2023-02-11 11:02:39 -0800

    Create entities and install  mongorepo package inside Catelog.API microservices

---
+++ src/Services/Catalog.API/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.API.Models
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string Summary { get; set; }
+        public string ImageFile { get; set; }
+        public decimal Price { get; set; }
+    }
+}
