d09d36966ba641382c7cfd565c65bb5f80b5e8f9
MD FAISAL <faisalcse1@gmail.com>
2023-03-02 02:22:25 +0600

    Managing PostgreeSQL Database Operations in Discount.Grpc microservice

--- src/Services/Discount.Grpc/Program.cs
+++ src/Services/Discount.Grpc/Program.cs
@@ -1,9 +1,12 @@
+using Discount.Grpc.Repository;
 using Discount.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Additional configuration is required to successfully run gRPC on macOS.
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+builder.Services.AddScoped<ICouponRepositroy, CouponRepository>();
 
 // Add services to the container.
 builder.Services.AddGrpc();
---
+++ src/Services/Discount.Grpc/Repository/ICouponRepositroy.cs
@@ -0,0 +1,12 @@
+﻿using Discount.Grpc.Models;
+
+namespace Discount.Grpc.Repository
+{
+    public interface ICouponRepositroy
+    {
+        Task<Coupon> GetDiscount(string productId);
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productId);
+    }
+}
