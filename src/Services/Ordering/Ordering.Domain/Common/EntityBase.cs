eec34ce13368c68a4d53922c84c2695bc165723a
MD FAISAL <faisalcse1@gmail.com>
2023-07-15 01:03:45 +0600

    added ordering microservice.

---
+++ src/Services/Ordering/Ordering.Domain/Common/EntityBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Common
+{
+    public abstract class EntityBase
+    {
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string UpdatedBy { get; set; }=string.Empty;
+        public DateTime? UpdatedDate { get; set;}
+    }
+}
---
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -0,0 +1,34 @@
+﻿using Ordering.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Models
+{
+    public class Order:EntityBase
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        //Billing Address
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+
+        //Payment
+        public string CardName { get; set; }
+        public string CardNumber { get; set; }
+        public string CVV { get; set; }
+        public string Expiraton { get; set; }
+        public int PaymentMethod { get; set; }
+
+    }
+}
