ef8475e913f4752b8e6e6ab6485db7f5cd7080c4
MD FAISAL <faisalcse1@gmail.com>
2023-09-09 01:43:56 +0600

    email send after placed order.

--- src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CreateOrder/CreateOrderCommandHandler.cs
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CreateOrder/CreateOrderCommandHandler.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using MediatR;
+using Ordering.Application.Contacts.Infrastructure;
 using Ordering.Application.Contacts.Persistence;
+using Ordering.Application.Models;
 using Ordering.Domain.Models;
 using System;
 using System.Collections.Generic;
@@ -14,15 +16,26 @@ namespace Ordering.Application.Features.Orders.Commands.CreateOrder
     {
         IOrderRepository _orderRepository;
         IMapper _mapper;
-        public CreateOrderCommandHandler(IOrderRepository orderRepository,IMapper mapper) 
-        { 
+        IEmailService _emailService;
+        public CreateOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper, IEmailService emailService)
+        {
             _orderRepository = orderRepository;
             _mapper = mapper;
+            _emailService = emailService;
         }
         public async Task<bool> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
         {
             var order = _mapper.Map<Order>(request);
-            return await _orderRepository.AddAsync(order);
+            bool isOrderPlaced = await _orderRepository.AddAsync(order);
+            if (isOrderPlaced)
+            {
+                Email email = new Email();
+                email.Subject = "Your Order has been placed.";
+                email.To = order.UserName;
+                email.Body = $"Dear {order.FirstName + " " + order.LastName} <br/><br/> We are excited for you to received your order #{order.Id} and with notify you one it's way. <br/> Thank you for ordering form Programming Palli.";
+                await _emailService.SendEmailAsync(email);
+            }
+            return isOrderPlaced;
         }
     }
 }
